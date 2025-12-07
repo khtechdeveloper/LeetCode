a94474f983973aa5704847855d50b6b7d0bf2d10
khtechdeveloper <khtechdeveloper@gmail.com>
2025-12-07 17:19:30 +0530

    This problem is to check Valid Parentheses or not.

---
+++ ValidParentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack();
+        int len=s.length();
+        for(int i=0;i<len;i++){
+            char ch=s.charAt(i);
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            }else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                char top=st.pop();
+                if(ch==')'&& top!='(' || ch=='}'&& top!='{' || ch==']' && top!='['){
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
