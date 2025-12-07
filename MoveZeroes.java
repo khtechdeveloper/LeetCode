c6f7433635265ece4106302a1003b89b43bbd4a3
khtechdeveloper <khtechdeveloper@gmail.com>
2025-12-07 17:26:24 +0530

    this problem is to move zeroes into an array.

---
+++ MoveZeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int length=nums.length;
+        int p1=0;
+        for(int p2=0;p2<nums.length;p2++){
+            if(nums[p2]!=0){
+                int temp=nums[p2];
+                nums[p2]=nums[p1];
+                nums[p1]=temp;
+                p1++;
+            }
+        }
+            
+        
+    }
+}
