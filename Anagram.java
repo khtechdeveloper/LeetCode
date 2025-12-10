0cc20db83934ebc422de04386923091a6943e003
khtechdeveloper <khtechdeveloper@gmail.com>
2025-12-10 17:42:40 +0530

    second way to solve Anagram solution.

--- Anagram.java
+++ Anagram.java
@@ -1,12 +1,30 @@
-public class Anagram {
-    public static void main(String[] args) {
-        String s="anagram", t="ramagan";
-        System.out.println(isAnagram(s,t));
+class Solution {
+    public static int asciiValue(String s){
+        int ascii=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            ascii+=(int)ch;
+        }
+        return ascii;
     }
-    public static  boolean isAnagram(String s, String t) {
-    
-            return s.sort().equals(t.sort());
-        
+    public boolean isAnagram(String s, String t) {
+        // char a[]=s.toCharArray();
+        // char b[]=t.toCharArray();
+        // Arrays.sort(a);
+        // Arrays.sort(b);
+
+        // if(Arrays.equals(a,b)){
+        //     return true;
+        // }else{
+        //     return false;
+        // }
+        // OR
+
+        if(asciiValue(s)==(asciiValue(t))){
+            return true;
+        }else{
+            return false;
+        }
 
     }
 }
