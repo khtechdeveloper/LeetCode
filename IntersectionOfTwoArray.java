40325fa6c8b91e774b6d15e399ffc56a073f2460
khtechdeveloper <khtechdeveloper@gmail.com>
2025-12-23 17:46:30 +0530

    this problem is to return an array which elements matches in both arrays.

---
+++ IntersectionOfTwoArray.java
@@ -0,0 +1,22 @@
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> result = new HashSet<>();
+        for (int n : nums1) {
+            set.add(n);
+        }
+        for (int n : nums2) {
+            if (set.contains(n)) {
+                result.add(n); 
+            }
+        }
+        int[] ans = new int[result.size()];
+        int i = 0;
+        for (int n : result) {
+            ans[i++] = n;
+        }
+        return ans;
+    }
+}
